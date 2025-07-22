c03caee6bd2f0db32d5f86f90fc5bdae019543b9
Matheusz.nied <matheustae@hotmail.com>
2025-07-22 07:23:22 -0300

    feat: appwrite config

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import cn from "clsx";
 import { Fragment } from "react";
 import { FlatList, Image, Pressable, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { images, offers } from "../constants";
+import { images, offers } from "../../constants";
 export default function Index() {
   
   return (
--- components/CartButton.tsx
+++ components/CartButton.tsx
@@ -1,18 +1,24 @@
 import { images } from "@/constants";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import React from 'react';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+// import {useCartStore} from "@/store/cart.store";
+import { router } from "expo-router";
 
-export default function CartButton() {
-    const totalItems = 10;
+const CartButton = () => {
+    // const { getTotalItems } = useCartStore();
+    // const totalItems = getTotalItems();
+    const totalItems = 0;
+
     return (
-        <TouchableOpacity className="cart-btn" onPress={() => {}}>
-            <Image source={images.bag} className="size-6" resizeMode="contain" />
-            {
-                totalItems > 0 && (
-                    <View className="cart-badge">
-                        <Text className="small-bold text-white">{totalItems}</Text>
-                    </View>
-                )
-            }
+        <TouchableOpacity className="cart-btn" onPress={()=> router.push('/cart')}>
+            <Image source={images.bag} className="size-5" resizeMode="contain" />
+
+            {totalItems > 0 && (
+                <View className="cart-badge">
+                    <Text className="small-bold text-white">{totalItems}</Text>
+                </View>
+            )}
         </TouchableOpacity>
     )
 }
+export default CartButton
