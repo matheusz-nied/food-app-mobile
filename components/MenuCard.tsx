bb2e1bda536d28809b3cf5359f51764f42e75f3b
Matheusz.nied <matheustae@hotmail.com>
2025-07-23 07:07:11 -0300

    fix: image of menu itens

--- components/MenuCard.tsx
+++ components/MenuCard.tsx
@@ -1,20 +1,17 @@
+import { useCartStore } from "@/store/cart.store";
 import { MenuItem } from "@/type";
 import { Image, Platform, Text, TouchableOpacity } from 'react-native';
-// import {appwriteConfig} from "@/lib/appwrite";
-// import {useCartStore} from "@/store/cart.store";
 
-const MenuCard = ({ item}: { item: MenuItem}) => {
-    // const imageUrl = `${image_url}?project=${appwriteConfig.projectId}`;
-    // const { addItem } = useCartStore();
-    const imageUrl = "image_url";
-    const addItem = (item: MenuItem) => {};
+const MenuCard = ({ item: { $id, name, price, image_url } }: { item: MenuItem}) => {
+    const imageUrl = `${image_url}`;
+    const { addItem } = useCartStore();
 
     return (
         <TouchableOpacity className="menu-card" style={Platform.OS === 'android' ? { elevation: 10, shadowColor: '#878787'}: {}}>
             <Image source={{ uri: imageUrl }} className="size-32 absolute -top-10" resizeMode="contain" />
-            <Text className="text-center base-bold text-dark-100 mb-2" numberOfLines={1}>{item.name}</Text>
-            <Text className="body-regular text-gray-200 mb-4">From ${item.price}</Text>
-            <TouchableOpacity onPress={() => addItem({...item})}>
+            <Text className="text-center base-bold text-dark-100 mb-2" numberOfLines={1}>{name}</Text>
+            <Text className="body-regular text-gray-200 mb-4">From ${price}</Text>
+            <TouchableOpacity onPress={() => addItem({ id: $id, name, price, image_url: imageUrl, customizations: []})}>
                 <Text className="paragraph-bold text-primary">Add to Cart +</Text>
             </TouchableOpacity>
         </TouchableOpacity>
